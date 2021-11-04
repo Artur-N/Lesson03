636dcf287a07d1982074bb899827af63074a257f
Artur <arturnl@ya.ru>
2021-11-04 22:26:22 +0300

    Task 06

---
+++ TASK06/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+int a;
+Console.WriteLine("Введите числ:  ");
+string s=Console.ReadLine();
+a=Convert.ToInt32(s);
+if (a%2==0)
+{
+    Console.WriteLine((a)+ " Чётное");
+}
+else
+{
+    Console.WriteLine((a)+ " Нечётное");
+}
