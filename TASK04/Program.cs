ca388622568f20e0471ee516932fcf4c2343d808
Artur <arturnl@ya.ru>
2021-11-04 22:25:57 +0300

    Task 04

---
+++ TASK04/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти максимальное из трех чисел
+int Max(int a, int b, int c)
+{
+    int max = 0;
+    if (a > b) max = a;
+    else max = b;
+    if (max > c) return max;
+    else return c;
+}
+Console.Write("MAX: ");
+Console.WriteLine(Max(31, 55, 14));
+
+Random rnd = new Random().Next(1, 10);
+Console.WriteLine(rnd);
