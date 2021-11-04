a98cf59148ce1d625c736a917c4ec24e0d10fd71
Artur <arturnl@ya.ru>
2021-11-04 22:25:42 +0300

    Task 03

---
+++ TASK03/Program.cs
@@ -0,0 +1,24 @@
+﻿// По заданному номеру дня недели вывести его название
+int a1=1;
+int a2=2;
+int a3=3;
+int a4=4;
+int a5=5;
+int a6=6;
+int a7=7;
+Console.WriteLine("Введите число дня недели:  ");
+string s=Console.ReadLine();
+a1=Convert.ToInt32(s);
+s=Console.ReadLine();
+a2=Convert.ToInt32(s);
+
+if (a>b)
+{
+   
+}
+else
+{
+  
+}
+
+Console.WriteLine(a1);
