9ab37f6fbe349c885096e5328243e8954eee6bf7
Artur <arturnl@ya.ru>
2021-11-05 03:03:50 +0300

    Task 07

---
+++ TASK07/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать числа от -N до N
+
+Console.WriteLine("Введите отрицательное число от -1 до -10: ");
+string s = Console.ReadLine();
+int a = Convert.ToInt32(s);
+Console.WriteLine("Введите положительное число от 0 до 10: ");
+s = Console.ReadLine();
+int b = Convert.ToInt32(s);
+
+int n = b - a + 1;
+int[] A = new int[n];
+int i = 0;
+while (A[i] < n)
+{
+    A[i] = a;
+    Console.Write(A[i] + " ");
+    a++;
+    i++;
+}
