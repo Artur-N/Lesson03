1520efe831dd6ae9aa7bd062875607c59aea2e85
Artur <arturnl@ya.ru>
2021-11-09 01:27:58 +0300

    task01 correct

--- TASK03/Program.cs
+++ TASK03/Program.cs
@@ -2,19 +2,35 @@
 int a;
 Console.WriteLine("Введите число дня недели:  ");
 a = Convert.ToInt32(Console.ReadLine ());
-if (a==1) 
-{Console.WriteLine ("Понедельник");}
-else if (a==2) 
-{Console.WriteLine ("Вторник");}
-else if (a==3) 
-{Console.WriteLine ("Среда");}
-else if (a==4) 
-{Console.WriteLine ("Четверг");}
-else if (a==5)
-{Console.WriteLine ("Пятница");}
-else if (a==6)
-{Console.WriteLine ("Суббота");}
-else if (a==7)
-{Console.WriteLine ("Воскресенье");}
-else 
-{Console.WriteLine ("Нет такого дня недели");}
+// if (a==1) {Console.WriteLine ("Понедельник");}
+// else if (a==2) {Console.WriteLine ("Вторник");}
+// else if (a==3) {Console.WriteLine ("Среда");}
+// else if (a==4) {Console.WriteLine ("Четверг");}
+// else if (a==5) {Console.WriteLine ("Пятница");}
+// else if (a==6) {Console.WriteLine ("Суббота");}
+// else if (a==7) {Console.WriteLine ("Воскресенье");}
+// else {Console.WriteLine ("Нет такого дня недели");}
+
+// switch (s)
+// {
+//     case "1":
+//     Console.WriteLine ("Понедельник");
+//     break;
+//     ...
+//     default:
+//     Console.WriteLine ("Нет такого дня недели");
+//     break;
+// }
+
+string[] days = new string[8];
+
+days[1] = "Понедельник";
+days[2] = "Вторник";
+int  day= Convert.ToInt32(s);
+
+// else if (a==3) {Console.WriteLine ("Среда");}
+// else if (a==4) {Console.WriteLine ("Четверг");}
+// else if (a==5) {Console.WriteLine ("Пятница");}
+// else if (a==6) {Console.WriteLine ("Суббота");}
+// else if (a==7) {Console.WriteLine ("Воскресенье");}
+// else {Console.WriteLine ("Нет такого дня недели");}
--- TASK07/Program.cs
+++ TASK07/Program.cs
@@ -1,19 +1,25 @@
 ﻿// Показать числа от -N до N
 
-Console.WriteLine("Введите отрицательное число от -1 до -10: ");
+Console.WriteLine("Введите число от 1 до 10: ");
 string s = Console.ReadLine();
 int a = Convert.ToInt32(s);
-Console.WriteLine("Введите положительное число от 0 до 10: ");
-s = Console.ReadLine();
-int b = Convert.ToInt32(s);
 
-int n = b - a + 1;
-int[] A = new int[n];
-int i = 0;
-while (A[i] < n)
+// Console.WriteLine("Введите положительное число от 0 до 10: ");
+// s = Console.ReadLine();
+// int b = Convert.ToInt32(s);
+
+// int n = b - a + 1;
+// int[] A = new int[n];
+// int i = 0;
+// while (A[i] < n)
+// {
+//     A[i] = a;
+//     Console.Write(A[i] + " ");
+//     a++;
+//     i++;
+// }
+
+for (int i=-a; i<=a; i++)
 {
-    A[i] = a;
-    Console.Write(A[i] + " ");
-    a++;
-    i++;
+    Console.Write(i + " ");
 }
