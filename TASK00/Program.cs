4ba841d76c3da7d6bf8c9006caddfd11e720c9dd
Artur <arturnl@ya.ru>
2021-11-09 02:09:39 +0300

    task05

--- TASK00/Program.cs
+++ TASK00/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Вывести квадрат числа
 double a;
 Console.Write("Введите число: ");
-string s=Console.ReadLine();
-a=Convert.ToInt32(s);
-Console.WriteLine(Math.Pow(a,2));
+string s = Console.ReadLine();
+a = Convert.ToInt32(s);
+Console.WriteLine(Math.Pow(a, 2));
--- TASK05/Program.cs
+++ TASK05/Program.cs
@@ -1,1 +1,5 @@
 ﻿// Написать программу вычисления значения функции y=f(a)
+double a = 5;
+double y;
+
+Console.WriteLine(y = Math.Pow(a,2)+1);
