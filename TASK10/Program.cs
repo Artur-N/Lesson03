1f6d6f921e409d50357c4e168c701677efb24d87
Artur <arturnl@ya.ru>
2021-11-10 01:59:43 +0300

    task10

---
+++ TASK10/Program.cs
@@ -0,0 +1,15 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трёхзначное число: ");
+double a = Convert.ToInt32(Console.ReadLine());
+double b;
+if (a >= 100 & a < 1000)
+{
+    b=((a%100)-(a%10))/10;
+    
+    Console.WriteLine("Последняя цифра: " + b);
+}
+else
+{
+    Console.WriteLine("Просили же ТРЁХЗНАЧНОЕ!!!");
+}
