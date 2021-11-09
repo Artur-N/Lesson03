2be74d3d28b89e033f5a150bb58242d561a8ef18
Artur <arturnl@ya.ru>
2021-11-10 01:12:30 +0300

    task03

--- TASK03/Program.cs
+++ TASK03/Program.cs
@@ -1,15 +1,28 @@
 ﻿// По заданному номеру дня недели вывести его название
+
 Console.WriteLine("Введите число дня недели:  ");
-string s = (Console.ReadLine());
-int a = Convert.ToInt32(s);
-if (a==1) {Console.WriteLine ("Понедельник");}
-else if (a==2) {Console.WriteLine ("Вторник");}
-else if (a==3) {Console.WriteLine ("Среда");}
-else if (a==4) {Console.WriteLine ("Четверг");}
-else if (a==5) {Console.WriteLine ("Пятница");}
-else if (a==6) {Console.WriteLine ("Суббота");}
-else if (a==7) {Console.WriteLine ("Воскресенье");}
-else {Console.WriteLine ("Нет такого дня недели");}
+int a = Convert.ToInt32(Console.ReadLine());
+
+// if (a==1) {Console.WriteLine ("Понедельник");}
+// else if (a==2) {Console.WriteLine ("Вторник");}
+// else if (a==3) {Console.WriteLine ("Среда");}
+// else if (a==4) {Console.WriteLine ("Четверг");}
+// else if (a==5) {Console.WriteLine ("Пятница");}
+// else if (a==6) {Console.WriteLine ("Суббота");}
+// else if (a==7) {Console.WriteLine ("Воскресенье");}
+// else {Console.WriteLine ("Нет такого дня недели");}
+
+string[] week = { "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс" };
+int i;
+if (a > 0 & a < 8)
+{
+    i = a - 1;
+    Console.WriteLine(week[i]);
+}
+else 
+{
+    Console.WriteLine ("Нет такого дня недели");
+}
 
 // switch (s)
 // {
