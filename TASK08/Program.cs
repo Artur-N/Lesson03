32e03cb48ea895c8b17de89444503bd8acb982f5
Artur <arturnl@ya.ru>
2021-11-10 00:48:30 +0300

    task08

---
+++ TASK08/Program.cs
@@ -0,0 +1,12 @@
+﻿// // 8. Показать четные числа от 1 до N
+
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i;
+for (i = 1; i <= n; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
