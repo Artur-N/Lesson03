8d14940b5d0c35660c9573a2f25340446063890c
Artur <arturnl@ya.ru>
2021-11-15 01:39:05 +0300

    Task 13

---
+++ TASK13/Program.cs
@@ -0,0 +1,15 @@
+﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int a = new Random().Next(1, 11);
+Console.WriteLine("Заданное число: " + a);
+Console.WriteLine("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a%b==0) 
+{
+    Console.WriteLine($"Число {b} кратно числу {a}");
+}
+else 
+{
+    Console.WriteLine($"Число {b} НЕ кратно числу {a}, остаток {a%b}");
+}
+
