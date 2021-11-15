74e9c5e38e783287d82d32dff7adf3a3e07b30ef
Artur <arturnl@ya.ru>
2021-11-15 21:50:44 +0300

    Task 35

---
+++ TASK35/Program.cs
@@ -0,0 +1,43 @@
+﻿// 35. Определить, присутствует ли в заданном массиве, некоторое число
+
+int[] arr = new int[10];
+int i = 0;
+for (i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(1, 100);
+    Console.Write($"{arr[i]} ");
+}
+Console.WriteLine();
+Console.Write("Введите число от 1 до 100: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+// bool flag = false;
+// for (i = 0; i < arr.Length; i++)
+// {
+//     if (digit == arr[i])
+//     {
+//         flag = true;
+//         break;
+//     }
+// }
+// if (flag == false)
+// {
+//     Console.Write($"Такого числа нет в массиве");
+// }
+// else
+// {
+//     Console.Write($"Чиcло {digit} присутствует в массиве");
+// }
+
+for (i = 0; i < arr.Length; i++)
+{
+    if (digit == arr[i])
+    {
+        Console.Write($"Чиcло {digit} присутствует в массиве");
+        break;
+    }
+}
+if (i == arr.Length)
+{
+    Console.Write($"Такого числа нет в массиве");
+}
