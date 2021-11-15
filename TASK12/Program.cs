236bec8186f938edea37ba1c2a67e1a9adab9e65
Artur <arturnl@ya.ru>
2021-11-15 21:52:59 +0300

    Task 12

--- TASK12/Program.cs
+++ TASK12/Program.cs
@@ -6,12 +6,15 @@ Console.WriteLine(triple);
 // 1й вариант
 string delete2 = Convert.ToString(triple);
 Console.WriteLine($"{delete2[0]}{delete2[2]}");
+Console.WriteLine(delete2[0] + "" + delete2[2]);
+Console.WriteLine("{0}{1}", delete2[0], delete2[2]);
 
 // 2й вариант
-int first = triple/100;
-int third = triple%10;
+int first = triple / 100;
+int third = triple % 10;
 Console.WriteLine($"{first}{third}");
 
 // 3й вариант
-int del2 = triple/100*10+triple%10;
+int del2 = triple / 100 * 10 + triple % 10;
 Console.WriteLine(del2);
+
