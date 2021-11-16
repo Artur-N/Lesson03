ab8a726d95b0351a60e960e70d762b3b8555a1b4
Artur <arturnl@ya.ru>
2021-11-17 00:10:00 +0300

    Task 22

--- TASK22/Program.cs
+++ TASK22/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 22. Найти расстояние между точками в пространстве 2D/3D
+
+ double Distance2D(double x1, double y1, double x2, double y2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+}
+Console.WriteLine(Distance2D(0, 0, 2, 2)); 
+ 
+// 22. Найти расстояние между точками в пространстве 3D
+
+double Distance3D(double x1, double y1, double z1, double x2, double y2, double Z2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(Z2 - z1, 2));
+}
+Console.WriteLine(Distance3D(0, 0, 0, 2, 2, 2));
