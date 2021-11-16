3fa6ad8563a6359f79bd25e150fb82efb5d7f65e
Artur <arturnl@ya.ru>
2021-11-16 22:15:25 +0300

    Task 19

--- TASK19/Program.cs
+++ TASK19/Program.cs
@@ -1,22 +1,18 @@
 ﻿// 19. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
 
 Console.WriteLine("Веедите координату Х");
-string s = Console.ReadLine();
-double x;
-x = Convert.ToInt32(s);
+double X = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Веедите координату Y");
-string s1 = Console.ReadLine();
-double Y;
-Y = Convert.ToInt32(s1);
+double Y = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Четверть N: " + Q(x, Y));
+Console.WriteLine("Четверть N: " + Q(X, Y));
 
 int Q(double x, double y)
 {
-    if (x > 0 && Y > 0) return 1;
-    if (x < 0 && Y > 0) return 2;
-    if (x < 0 && Y < 0) return 3;
-    if (x > 0 && Y < 0) return 4;
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
     return -1;
 }
