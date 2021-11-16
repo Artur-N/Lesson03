f32e431937fb348894b50c116eb838efccacf159
Artur <arturnl@ya.ru>
2021-11-16 17:38:31 +0300

    Task 18

--- TASK18/Program.cs
+++ TASK18/Program.cs
@@ -1,7 +1,6 @@
 ﻿// 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
 bool x = false;
-bool y = false;
+bool y = true;
 bool f1 = !(x || y);
 bool f2 = !x && !y;
 Console.WriteLine(f1 == f2);
-
