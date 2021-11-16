11f0251c6f50067b2540ed309cee16f46168bbbf
Artur <arturnl@ya.ru>
2021-11-16 23:58:02 +0300

    Task 21

---
+++ TASK21/Program.cs
@@ -0,0 +1,13 @@
+﻿// 21. Программа проверяет пятизначное число на палиндромом.
+
+string pol(int a)
+{
+    string b = Convert.ToString(a);
+    string result = $"Число {a} НЕ является палиндромом";
+    if (b[0] == b[4] && b[1] == b[3]) result = $"Число {a} является палиндромом";
+    return result;
+}
+
+Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(pol(num));
