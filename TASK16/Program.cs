84d4ccfb31798fbc9239bc315d5ad31dd623832e
Artur <arturnl@ya.ru>
2021-11-16 15:26:15 +0300

    Task 16

---
+++ TASK16/Program.cs
@@ -0,0 +1,8 @@
+﻿// 16. Дано число обозначающее день недели. Выяснить является номер недели выходным днём
+
+Console.WriteLine("Введите день недели: ");
+int a = Convert.ToInt32(Console.ReadLine()); // new Random().Next(1, 8);
+
+if (a == 6 || a == 7) Console.WriteLine("Выходной!");
+else if (a < 6) {Console.WriteLine("Рабочий день");}
+else {Console.WriteLine("Нет такого дня недели");}
