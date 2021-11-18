4e78f2910c0719e728a2881a448134db50a2d5ca
Artur <arturnl@ya.ru>
2021-11-19 01:49:58 +0300

    Task 28

---
+++ TASK28/Program.cs
@@ -0,0 +1,16 @@
+﻿// 28. Подсчитать сумму цифр в числе
+
+int a = new Random().Next(-100000, 100000);
+Console.WriteLine(a);
+
+int Sum(int s)
+{
+    int summa;
+    for (summa = 0; s != 0;)
+    {
+        summa = summa + s % 10;
+        s = s / 10;
+    }
+    return summa;
+}
+Console.WriteLine(Sum(a));
