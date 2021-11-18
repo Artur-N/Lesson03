c293f67d4393050ddf67ed47045de3a656ee1cd5
Artur <arturnl@ya.ru>
2021-11-18 21:50:22 +0300

    Task 25

---
+++ TASK25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти сумму чисел от 1 до А
+
+int metod(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum = i + sum;
+    }
+    return sum;
+}
+    Console.WriteLine("Введите число: ");
+    int A = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(metod(A));
