dd6f02cd165295b5556c5e6d8d85ecbe999cca95
Artur <arturnl@ya.ru>
2021-11-18 21:50:09 +0300

    Task 23

---
+++ TASK23/Program.cs
@@ -0,0 +1,19 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N
+
+int[] pow2(int n)
+{
+    int[] tabl = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        tabl[i] = (int)Math.Pow(i+1, 3);
+    }
+    return tabl;
+}
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] arr = pow2(N);
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write($"Квадраты {arr[i]}");
+}
