f14fbf1586b25ca7f123c1e990377fc212737fc8
Artur <arturnl@ya.ru>
2021-11-19 02:33:53 +0300

    Task 29

---
+++ TASK29/Program.cs
@@ -0,0 +1,14 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+
+uint P(int a)
+{
+    uint s = 1;
+    for (uint i = 1; i <= a; i++)
+    {
+        s = s * i;
+    }
+    return s;
+}
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Произведение чисел от 1 до {N} = {P(N)}");
