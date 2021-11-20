0fc2c1cbeb8d127579210f393266477fc3b559a9
Artur <arturnl@ya.ru>
2021-11-21 01:40:39 +0300

    Task 30 correct

--- TASK30/Program.cs
+++ TASK30/Program.cs
@@ -1,33 +1,51 @@
 ﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
 
+// int[] kub = new int[10];
+// for (int i = 0; i < kub.Length; i++)
+// {
+//     kub[i] = new Random().Next(1, 10);
+//     if (kub[i] % 2 == 0)
+//     {
+//         Console.WriteLine($"{kub[i]} в кубе = {Math.Pow(kub[i], 3)}");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{kub[i]} в кубе - заканчивается на нечётную цифру");
+//     }
+// }
+
+// Решение через методы
+
 int[] metod3(int[] arr)
 {
-    int[] arr2 = new int[arr.Length];
-    int j = 0;
+    // int[] arr2 = new int[arr.Length];
+    // int j = 0;
     for (int i = 0; i < arr.Length; i++)
     {
+        arr[i] = new Random().Next(1, 10);
+        Console.Write(arr[i] + " ");
         arr[i] = (int)Math.Pow(arr[i], 3);
-        if (arr[i] % 2 == 0)
-        {
-            arr2[j] = arr[i];
-            j++;
-        }
+        // if (arr[i] % 2 == 0)
+        // {
+        //     arr2[j] = arr[i];
+        //     j++;
+        // }
     }
-    return arr2;
+    return arr;
 }
 
-int[] array = new int[10];
-Random rnd = new Random();
-for (int j = 0; j < array.Length; j++)
+void print(int[] arr)
 {
-    array[j] = rnd.Next(1, 10);
-    Console.Write(array[j] + " ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) Console.Write(arr[i] + " ");
+    }
 }
+
+int[] even = new int[10];
+
+metod3(even);
 Console.WriteLine();
-int[] even = metod3(array);
-for (int i = 0; i < even.Length; i++)
-{
-    Console.Write(even[i] + " ");
-}
+print(even);
 
 
