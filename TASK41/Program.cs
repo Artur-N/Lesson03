bc83faeade3acd2f438b60be38fac49110be66a9
Artur <arturnl@ya.ru>
2021-11-30 01:53:37 +0300

    task 41

---
+++ TASK41/Program.cs
@@ -0,0 +1,13 @@
+﻿// 41. Выяснить являются ли три числа сторонами треугольника
+
+string ThreeNumbers(int a, int b, int c)
+{
+    string result;
+    if (a + b > c && b + c > a && a + c > b) result = "Являются";
+    else result = "Не являются";
+    return result;
+}
+int a = 5;
+int b = 3;
+int c = 4;
+Console.WriteLine(ThreeNumbers(a, b, c));
