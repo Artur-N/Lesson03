e793281ce7adbdf3793fe20da38a1443dc5607c8
Artur <arturnl@ya.ru>
2021-11-30 02:52:27 +0300

    task 42

---
+++ TASK42/Program.cs
@@ -0,0 +1,25 @@
+﻿// 42. Определить сколько чисел больше 0 введено с клавиатуры
+
+int UpZero(int[] a)
+{
+    int count = 0;
+    for (int i = 0; i < a.Length; i++)
+    {
+        if (a[i] > 0) count++;
+    }
+    return count;
+}
+
+int[] CreateArray(int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-е число");
+        a[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return a;
+}
+
+int[] Array = new int[5];
+CreateArray(Array);
+Console.WriteLine($"Чисел больше 0 введено - {UpZero(Array)} шт");
