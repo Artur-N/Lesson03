c0fe2add2ee5bf2a427ce5bcfbbe6f8782905cb5
Artur <arturnl@ya.ru>
2021-11-30 01:06:36 +0300

    task 40 correct

--- TASK40/Program.cs
+++ TASK40/Program.cs
@@ -1,43 +1,48 @@
 ﻿// 40. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
 
-double[] RndArr(double[] Arr)
+Random rnd = new Random();
+double[] RandomArray(double[] Arr)
 {
+
     for (int i = 0; i < Arr.Length; i++)
     {
-        Arr[i] = new Random().NextDouble() * 10;
+        Arr[i] = rnd.NextDouble() * 10;
     }
     return Arr;
 }
 
-double MaxMin(double[] Arr)
+double MaxMin(double[] Arr, out double max, out double min)
 {
     int i = 0;
-    double res;
-    double max = Arr[i];
-    double min = Arr[i];
+    double result;
+    max = Arr[i];
+    min = Arr[i];
     for (i = 0; i < Arr.Length; i++)
     {
         if (Arr[i] > max) max = Arr[i];
         if (Arr[i] < min) min = Arr[i];
     }
-    Console.WriteLine("Максимальное значение = {0:f}", max);
-    Console.WriteLine("Минимальное значение = {0:f}", min);
-    res = max - min;
-    return res;
+    result = max - min;
+    return result;
 }
 
-void Print(double[] Arr)
+void PrintArray(double[] Arr)
 {
     for (int i = 0; i < Arr.Length; i++)
     {
-        Console.WriteLine("{0:f}", Arr[i]);
+        Console.Write("{0:f} | ", Arr[i]);
     }
 }
 
 double[] Array = new double[10];
-RndArr(Array);
-Print(Array);
-Console.WriteLine("Разница между макс и мин значениями массива = {0:f}", MaxMin(Array));
+RandomArray(Array);
+PrintArray(Array);
+double max, min;
+double result = MaxMin(Array, out max, out min);
+Console.WriteLine();
+Console.WriteLine("Максимальное значение = {0:f}", max);
+Console.WriteLine("Минимальное значение = {0:f}", min);
+Console.WriteLine("Разница между макс и мин значениями массива = {0:f}", result);
 
 
 
