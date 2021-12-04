5d890a4913a47719c6ce83190f2462eca2e6e4d2
Artur <arturnl@ya.ru>
2021-12-04 15:59:15 +0300

    Task 43, 44, 45

---
+++ TASK43/Program.cs
@@ -0,0 +1,6 @@
+﻿// // 43. Написать программу преобразования десятичного числа в двоичное
+
+Console.Write("Введите число :");
+int digit10 = Convert.ToInt32(Console.ReadLine());
+string digit2 = Convert.ToString(digit10, 2);
+Console.WriteLine($" В двоичной системе исчесления : {digit2}");
---
+++ TASK45/Program.cs
@@ -0,0 +1,24 @@
+﻿// 45. Показать числа Фибоначчи
+
+int[] Fiboncchi(int[] array)
+{
+    array[1] = 1;
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+
+void Print(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int[] arrayF = new int[30];
+Fiboncchi(arrayF);
+Print(arrayF);
